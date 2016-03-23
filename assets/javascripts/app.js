0a8963b766999928cb5de4e2ab58e1c8f0fb418a
ValadierThomas <1.thomas.valadier@orange.fr>
2016-03-23 20:36:06 +0100

    premier commit

---
+++ assets/javascripts/app.js
@@ -0,0 +1,41 @@
+(function($){
+
+  $('.addPanier').click(function(event){
+    // event.preventDefault();
+    $.get($(this).attr('href'),{},function(data){
+      if(data.error){
+        alert(data.message);
+      }else{
+        if(confirm(data.message + '. Voulez-vous consulter votre panier ?')){
+          location.href = '../pages/panier.php';
+        }else{
+          //$('#total').empty().append(data.total);
+          $('#count').empty().append(data.count);
+        }
+      }
+    },'json');
+    return false;
+  });
+
+})(jQuery);
+
+(function($){
+
+  $('.plusPanier').click(function(event){
+    // event.preventDefault();
+    $.get($(this).attr('href'),{},function(data){
+      if(data.error){
+        alert(data.message);
+      }else{
+        if(confirm(data.message + '. Voulez-vous consulter votre panier ?')){
+          location.href = '../panier.php';
+        }else{
+          //$('#total').empty().append(data.total);
+          $('#count').empty().append(data.count);
+        }
+      }
+    },'json');
+    return false;
+  });
+
+})(jQuery);
